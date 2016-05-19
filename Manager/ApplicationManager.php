cdde91d3f066193a0d807dbba6e6ba35533375e8
Genar Trias Ortiz <genar@acs.li>
2016-05-19 19:10:20 +0200

    successful oauth2 login to BeaconControl API

--- Manager/ApplicationManager.php
+++ Manager/ApplicationManager.php
@@ -4,6 +4,21 @@ namespace Cirici\BeaconControlClientBundle\Manager;
 
 class ApplicationManager
 {
+    private $client;
+    private $clientId;
+    private $clientSecret;
+    private $adminEmail;
+    private $adminPassword;
+
+    public function __construct($client, $clientId, $clientSecret, $adminEmail, $adminPassword)
+    {
+        $this->client = $client;
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->adminEmail = $adminEmail;
+        $this->adminPassword = $adminPassword;
+    }
+
     /**
      * getApplications
      *
@@ -12,5 +27,34 @@ class ApplicationManager
      */
     public function getApplications()
     {
+        $accessToken = $this->getAccessToken();
+
+        $result = $this->client->get('applications.json', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $accessToken->access_token
+            ]
+        ]);
+        return json_decode((string) $result->getBody());
+    }
+
+    /**
+     * getAccessToken
+     *
+     * @access public
+     * @return string
+     */
+    public function getAccessToken()
+    {
+        $result = $this->client->post('oauth/token', [
+            'form_params' => [
+                'client_id' => $this->clientId,
+                'client_secret' => $this->clientSecret,
+                'grant_type' => 'password',
+                'email' => $this->adminEmail,
+                'password' => $this->adminPassword,
+            ]
+        ]);
+
+        return json_decode((string) $result->getBody());
     }
 }
--- Tests/Manager/ApplicationManagerTest.php
+++ Tests/Manager/ApplicationManagerTest.php
@@ -2,11 +2,15 @@
 
 namespace Cirici\JWTClientBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class ApplicationManagerTest extends WebTestCase
 {
     public function testGetApplications()
     {
+        $client = static::makeClient();
+        $applicationManager = $client->getContainer()->get('cirici_beacon_control_client.application_manager');
+        $applications = $applicationManager->getApplications();
+        $this->assertNotNull($applications);
     }
 }
