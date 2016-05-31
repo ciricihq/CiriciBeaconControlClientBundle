d31fa0003b254397666a260ae58e440032aa1173
Genar Trias Ortiz <genar@acs.li>
2016-05-31 12:04:25 +0200

    successful save of beacons lat and lng

--- Manager/BeaconManager.php
+++ Manager/BeaconManager.php
@@ -31,4 +31,28 @@ class BeaconManager
         ]);
         return json_decode((string) $result->getBody());
     }
+
+    /**
+     * updateBeaconPosition
+     *
+     * @param mixed $beaconId
+     * @param array $position
+     * @access public
+     * @return void
+     */
+    public function updateBeaconPosition($beaconId, array $position)
+    {
+        $accessToken = $this->authManager->getAccessToken();
+
+        $result = $this->client->put('beacons/' . $beaconId . '.json', [
+            'form_params' => [
+                'beacon[lat]' => $position['lat'],
+                'beacon[lng]' => $position['lng'],
+            ],
+            'headers' => [
+                'Authorization' => 'Bearer ' . $accessToken->access_token
+            ]
+        ]);
+        return json_decode((string) $result->getBody());
+    }
 }
