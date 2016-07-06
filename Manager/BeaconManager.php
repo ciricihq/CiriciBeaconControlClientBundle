d9f999916ab48c7c4b0c4a0c82a011881064fcc6
Genar Trias Ortiz <genar@acs.li>
2016-07-06 13:50:25 +0200

    adding postBeacon and deleteBeacon methods

--- Manager/BeaconManager.php
+++ Manager/BeaconManager.php
@@ -2,6 +2,8 @@
 
 namespace Cirici\BeaconControlClientBundle\Manager;
 
+use GuzzleHttp\Exception\RequestException;
+
 class BeaconManager
 {
     private $client;
@@ -49,6 +51,73 @@ class BeaconManager
                 'beacon[lat]' => $position['lat'],
                 'beacon[lng]' => $position['lng'],
             ],
+            'headers' => [
+                'Authorization' => 'Bearer ' . $accessToken->access_token
+            ]
+        ]);
+        return json_decode((string) $result->getBody());
+    }
+
+    /**
+     * postBeacon
+     *
+     * @access public
+     * @return stdObject
+     */
+    public function postBeacon($beacon)
+    {
+        $accessToken = $this->authManager->getAccessToken();
+
+        try {
+            $result = $this->client->post('beacons.json', [
+                'form_params' => [
+                    'beacon[name]' => $beacon['name'],
+                    'beacon[uuid]' => $beacon['uuid'],
+                    'beacon[major]' => $beacon['major'],
+                    'beacon[minor]' => $beacon['minor'],
+                    // 'beacon[lat]' => $beacon['lat'],
+                    // 'beacon[lng]' => $beacon['lng'],
+                    // 'beacon[floor]' => $beacon['zone_id'],
+                ],
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $accessToken->access_token
+                ]
+            ]);
+        } catch (RequestException $e) {
+            return json_decode((string) $e->getResponse()->getBody());
+        }
+        return json_decode((string) $result->getBody());
+    }
+
+    /**
+     * deleteBeacon
+     *
+     * @param mixed $id
+     * @access public
+     * @return void
+     */
+    public function deleteBeacon($id)
+    {
+        $accessToken = $this->authManager->getAccessToken();
+
+        try {
+            $result = $this->client->delete('beacons/' . $id . '.json', [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $accessToken->access_token
+                ]
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return json_decode((string) $e->getResponse()->getBody());
+        }
+
+        return json_decode((string) $result->getBody());
+    }
+
+    public function retrieveBeacon($name)
+    {
+        $accessToken = $this->authManager->getAccessToken();
+
+        $result = $this->client->get('beacons.json?name=' . $name, [
             'headers' => [
                 'Authorization' => 'Bearer ' . $accessToken->access_token
             ]
